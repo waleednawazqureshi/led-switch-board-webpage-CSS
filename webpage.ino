946a950c9dd768d0e059e3267964aebb6846e3af
waleednawazqureshi <94011007+waleednawazqureshi@users.noreply.github.com>
2021-11-13 11:03:15 -0800

    Create webpage.ino

---
+++ webpage.ino
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>project</title>
+    
+</head>
+<h1> Smart Home</h1>
+<style>
+   body{ text-align: center;
+    color:white;
+    background-image: url(https://cdn.pixabay.com/photo/2017/08/30/01/05/milky-way-2695569__480.jpg);
+    background-position: center;
+    background-size: cover;
+   }
+
+button{
+    padding: 5px 25px;
+    color:black;
+    margin:  10px;
+}
+    
+</style>
+
+<body>
+    <h1> IOT HOME SWITCH BOARD</h1>
+    <h2> Welcome To IOT Appliance Control Homepage</h2>
+    <h3> Press ON To Trun of Switch & OFF The Switch</h3>
+    <p>
+        SWITCH 01 Status OFF <br>
+        <button width="50px">ON</button>
+        <BUtton>OFF</BUtton>
+    </p> <br>
+
+    <p> SWITCH 02 Status ON <br>
+        <button> ON </button>
+        <button> OFF </button>
+    </p><br>
+    <p> SWITCH 03 Status OFF <br>
+        <button>ON</button>
+        <button>OFF</button>
+    </p> <br>
+    <p>SWITCH 04 Status ON <br>
+        <button>ON</button>
+        <button>OFF</button>
+    </p>
+</body>
+
+</html>
